0118b1778c47cb0f24127d401f165a26ef3170f2
Barracudach <javay999@mail.ru>
2022-08-02 03:29:52 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # CallStack-Spoofer
+
+A simple tool that will make it harder to find your code in the call stack.
+This works both in the kernel-mode and in the user-mode.
+Safe with runtime exceptions.
+
+[IDA output when compiling first line](# PoC)
+
+### example in user-mode
+```cpp
+SPOOF_FUNC;//spoof current function
+SPOOF_CALL(Beep)(500, 500);//spoof system call
+```
+
+### example in kernel-mode
+```cpp
+SPOOF_FUNC;//spoof current function
+SPOOF_CALL(ULONG,Beep)(500, 500);//spoof system call
+```
+### presetting
+For kernel- disable Control Flow Guard (CFG) /guard:cf 
+Developed for the x64 platform.
+Requires changes for x32.
+
+### c++ standart
+Usermode c++17 and above
+Uernelmode c++14 and above
+
+### PoC
+
+![Call stack with spoofer enabled](relative/path/in/repository/to/image.svg)
+
+![Call stack with spoofer disabled](relative/path/in/repository/to/image.svg)
+
+There are 2 modes of operation:
+- Erase the return address of the current function and restore it when the destructor is called during the return of the function. Since we are using a class with a local scope, we will call the destructor anyway. This also works with exceptions.
+- Generate a shellcode that will proxy our system calls, as a result of which the return address will not point to our code.
+
+While the first case is simple, the second case is a bit more complicated. What should we know? The function template generates a function at compile time with certain parameters that we call it with in the code. Subsequent calls will use the already generated function. Therefore, we can get the address of the generated function like this:
+```cpp
+	PVOID self_addr = static_cast<PVOID>(&ShellCodeGenerator<RetType, Func*, Args&&...>);
+```
+With each subsequent same call, the address will match. Therefore, we can cache our matched shellcode and template function address pairs so that we only allocate the shellcode once and then use it. For backwards compatibility with the kernel, no standard containers are used.
+
+It is important to understand that no function calls should be used in the shellcode, except for calls via direct addresses, because the calls use **nearcall**, and we will get an invalid relative call.
+We also encrypt our return address with xor , because it will still be stored on the stack.
---
+++ include/CallStack-Spoofer.h
@@ -0,0 +1,239 @@
+#pragma once
+#ifdef _KERNEL_MODE
+#include <ntddk.h>
+#include <ntdef.h>
+#include <xtr1common> 
+#else
+#include <utility>
+#endif
+#include  <Intrin.h> 
+
+
+/*
+ *  Copyright 2022 Barracudach
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ //From repository: https://github.com/Barracudach/CallStack-Spoofer
+ //Supports 2 modes: kernelmode and usermode(x64)
+ //For kernel- disable Control Flow Guard (CFG) /guard:cf 
+ //usermode c++17 and above
+ //kernelmode c++14 and above
+
+
+
+#define SPOOF_FUNC CallSpoofer::SpoofFunction spoof(_AddressOfReturnAddress());
+#ifdef _KERNEL_MODE
+#define SPOOF_CALL(ret_type,name) (CallSpoofer::SafeCall<ret_type,decltype(name)>(&name))
+#else
+#define SPOOF_CALL(name) (CallSpoofer::SafeCall<decltype(name)>(&name))
+#endif
+
+
+#define MAX_FUNC_BUFFERED 100
+#define SHELLCODE_GENERATOR_SIZE 500
+
+namespace CallSpoofer
+{
+#ifdef _KERNEL_MODE
+#pragma region std::forward
+	template <class _Ty>
+	struct remove_reference {
+		using type = _Ty;
+		using _Const_thru_ref_type = const _Ty;
+	};
+	template <class _Ty>
+	using remove_reference_t = typename remove_reference<_Ty>::type;
+
+	template <class>
+	constexpr bool is_lvalue_reference_v = false; // determine whether type argument is an lvalue reference
+
+	template <class _Ty>
+	constexpr bool is_lvalue_reference_v<_Ty&> = true;
+
+	template <class _Ty>
+	constexpr _Ty&& forward(
+		remove_reference_t<_Ty>& _Arg) noexcept { // forward an lvalue as either an lvalue or an rvalue
+		return static_cast<_Ty&&>(_Arg);
+	}
+
+	template <class _Ty>
+	constexpr _Ty&& forward(remove_reference_t<_Ty>&& _Arg) noexcept { // forward an rvalue as an rvalue
+		static_assert(!is_lvalue_reference_v<_Ty>, "bad forward call");
+		return static_cast<_Ty&&>(_Arg);
+	}
+#pragma endregion 
+
+#else
+	using namespace std;
+#endif
+
+}
+
+
+namespace CallSpoofer
+{
+	typedef unsigned __int64  uintptr_t, size_t;
+
+	class SpoofFunction
+	{
+	public:
+		uintptr_t temp = 0;
+		const uintptr_t xor_key = 0xff00ff00ff00ff00;
+		void* ret_addr_in_stack = 0;
+
+		SpoofFunction(void* addr) :ret_addr_in_stack(addr)
+		{
+			temp = *(uintptr_t*)ret_addr_in_stack;
+			temp ^= xor_key;
+			*(uintptr_t*)ret_addr_in_stack = 0;
+		}
+		~SpoofFunction()
+		{
+			temp ^= xor_key;
+			*(uintptr_t*)ret_addr_in_stack = temp;
+		}
+	};
+
+#ifdef _KERNEL_MODE
+	__forceinline PVOID LocateShellCode(PVOID func, size_t size = 500)
+	{
+		void* addr = ExAllocatePoolWithTag(NonPagedPool, size, (ULONG)"File");
+		if (!addr)
+			return nullptr;
+		return memcpy(addr, func, size);
+	}
+#else
+	__forceinline PVOID LocateShellCode(PVOID func, size_t size = SHELLCODE_GENERATOR_SIZE)
+	{
+		void* addr = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+		if (!addr)
+			return nullptr;
+		return memcpy(addr, func, size);
+	}
+#endif
+
+#ifdef _KERNEL_MODE
+	template <typename RetType, typename Func, typename ...Args>
+	RetType
+#else
+	template <typename Func, typename ...Args>
+	typename std::invoke_result<Func, Args...>::type
+#endif
+		__declspec(safebuffers)ShellCodeGenerator(Func f, Args&... args)
+	{
+#ifdef _KERNEL_MODE
+		using this_func_type = decltype(ShellCodeGenerator<RetType, Func, Args&...>);
+		using return_type = RetType;
+#else
+		using this_func_type = decltype(ShellCodeGenerator<Func, Args&...>);
+		using return_type = typename std::invoke_result<Func, Args...>::type;
+#endif
+		const uintptr_t xor_key = 0xff00ff00ff00ff00;
+		void* ret_addr_in_stack = _AddressOfReturnAddress();
+		uintptr_t temp = *(uintptr_t*)ret_addr_in_stack;
+		temp ^= xor_key;
+		*(uintptr_t*)ret_addr_in_stack = 0;
+
+		if constexpr (std::is_same<return_type, void>::value)
+		{
+			f(args...);
+			temp ^= xor_key;
+			*(uintptr_t*)ret_addr_in_stack = temp;
+		}
+		else
+		{
+			return_type&& ret = f(args...);
+			temp ^= xor_key;
+			*(uintptr_t*)ret_addr_in_stack = temp;
+			return ret;
+		}
+	}
+
+
+
+#ifdef _KERNEL_MODE
+	template<typename RetType, class Func >
+#else
+	template<class Func >
+#endif
+	class SafeCall
+	{
+		Func* funcPtr;
+
+	public:
+		SafeCall(Func* func) :funcPtr(func) {}
+
+		template<typename... Args>
+		__forceinline decltype(auto) operator()(Args&&... args)
+		{
+			SPOOF_FUNC;
+
+#ifdef _KERNEL_MODE
+			using return_type = RetType;
+			using shell_code_generator_type = decltype(&ShellCodeGenerator<RetType, Func*, Args...>);
+			PVOID self_addr = static_cast<PVOID>(&ShellCodeGenerator<RetType, Func*, Args&&...>);
+#else	
+			using return_type = typename std::invoke_result<Func, Args...>::type;
+			using shell_code_generator_type = decltype(&ShellCodeGenerator<Func*, Args...>);
+			PVOID self_addr = static_cast<PVOID>(&ShellCodeGenerator<Func*, Args&&...>);
+#endif
+
+			shell_code_generator_type p_shellcode{};
+
+			static size_t count{};
+			static PVOID orig_generator[MAX_FUNC_BUFFERED]{};
+			static PVOID alloc_generator[MAX_FUNC_BUFFERED]{};
+
+			unsigned index{};
+			while (orig_generator[index])
+			{
+				if (orig_generator[index] == self_addr)
+				{
+#ifdef _KERNEL_MODE
+					//DbgPrint("Found allocated generator");
+#else
+					//std::cout << "Found allocated generator" << std::endl;
+#endif
+					p_shellcode = reinterpret_cast<shell_code_generator_type>(alloc_generator[index]);
+					break;
+				}
+				index++;
+			}
+
+			if (!p_shellcode)
+			{
+#ifdef _KERNEL_MODE
+				//DbgPrint("Alloc generator");
+#else	
+				//std::cout << "Alloc generator" << std::endl;
+#endif
+				p_shellcode = (shell_code_generator_type)LocateShellCode(self_addr);
+				orig_generator[count] = self_addr;
+				alloc_generator[count] = p_shellcode;
+				count++;
+			}
+
+			if (!p_shellcode)
+			{
+				//и все и пиздец
+				//DbgPrint("!p_shellcode");
+
+			}
+			return p_shellcode(funcPtr, args...);
+		}
+	};
+}
+
+
